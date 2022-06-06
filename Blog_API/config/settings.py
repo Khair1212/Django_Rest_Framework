54658074b6835c6468d7b6801fd517535ef100f1
Khair Ahammed <khairahmad6@gmail.com>
2022-06-07 01:57:43 +0600

    6th chapter: Permissions

--- Blog_API/config/settings.py
+++ Blog_API/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4fnr8gdj#pu(k_i=0dd1s4rh0s8w8sx9ictu4yibcdxg^x1qxn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
 # new
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES':[
-        'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.IsAuthenticated',
     ]
 }
 
