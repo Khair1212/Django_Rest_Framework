d90b1d64ed88abbc0b12254493cac3f0acf365f6
Khair Ahammed <khairahmad6@gmail.com>
2022-06-08 02:35:06 +0600

    Chapter 7: Authentication

--- Blog_API/posts/views.py
+++ Blog_API/posts/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+# from rest_framework import permissions
 from .models import Post
+from .permissions import IsAuthorOrReadOnly
 from .serializers import PostSerializer
 # Create your views here.
 
 
 class PostList(ListCreateAPIView):
+    # permission_classes = (permissions.IsAuthenticated,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
+
 class PostDetail(RetrieveUpdateDestroyAPIView):
+    # permission_classes = (permissions.IsAuthenticated,)
+    permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
